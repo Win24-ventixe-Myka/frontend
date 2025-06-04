bd80683d42871031f57ca0919031acf040b0d072
msulinx <msulin0329@hotmail.com>
2025-06-05 00:42:49 +0200

    .

--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -5,34 +5,6 @@ import AllEvents from '../components/AllEvents'
 
 const Dashboard = () => {
 
-  const fetchEventsWithToken = async () => {
-    const token = localStorage.getItem('token'); // hämta token
-    if (!token) {
-      console.error("No token found, can't fetch events");
-      return;
-    }
-
-    try {
-      const res = await fetch("https://eventservice-mvp-deawdpaqc7d8c9hq.swedencentral-01.azurewebsites.net/api/events", {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        }
-      });
-      if (res.ok) {
-        const data = await res.json();
-        console.log("Events response:", data);
-      } else {
-        console.error("Failed to fetch events, status:", res.status);
-      }
-    } catch (error) {
-      console.error("Fetch events error:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchEventsWithToken();
-  }, []);
-
   return (
 
 <div id="dashboard">
